dc1dcad9cde0ac0216c701f0af9cc7a57bb2c149
Batu Teköz <137097693+batutekoz77@users.noreply.github.com>
2026-02-16 15:22:04 +0100

    Add files via upload

---
+++ Source/Http/Http.hpp
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "HTTP/Http_Extension.h"
+
+class HttpManager {
+public:
+    bool Fetcher();
+    void Injector();
+
+public:
+    bool fetching = false;
+    std::string server_data_cache;
+
+private:
+    static auto getValue(const std::string& src, const std::string& key) -> std::string {
+        std::string search = key + "|";
+        size_t pos = src.find(search);
+        if (pos == std::string::npos) return "";
+
+        pos += search.length();
+        size_t end = src.find("\n", pos);
+        if (end == std::string::npos) end = src.length();
+
+        return src.substr(pos, end - pos);
+    }
+
+};
+extern HttpManager Http;
