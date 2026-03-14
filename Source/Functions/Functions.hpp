e533928bdcc2e70a93671939988d5ca41c690d08
Batu Teköz <137097693+batutekoz77@users.noreply.github.com>
2026-03-14 17:31:03 +0100

    Add files via upload

---
+++ Source/Functions/Functions.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+
+namespace System {
+	bool RelaunchAsAdmin();
+	bool IsTrueAdmin();
+	bool EnableDebugPrivilege();
+	bool editHosts(std::string ipAddress);
+	int findProcess(const std::wstring& program);
+	void endProcess(const std::wstring& program);
+	void startProcess(const std::wstring& program);
+}
+
+namespace Packet {
+	bool Contains(const std::string& packet, const std::string& key);
+
+	template <typename T>
+	bool ContainsValue(const std::string& packet, const std::string& key);
+
+    template <typename T>
+	T ExtractValue(const std::string& packet, const std::string& key, T defaultValue);
+
+	template<typename T>
+	T ExtractCustom(const std::string& data, const std::string& starter, size_t startIndex, const std::string& delimiter);
+
+	bool Change(std::string& data, const std::string& starter, size_t startIndex, const std::string& delimiter, const std::string& newValue);
+}
